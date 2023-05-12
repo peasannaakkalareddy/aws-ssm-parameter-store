d8d9a32f628a601739e6b4a56b1c221a230892dd
prasanna kumar <prasannaakkalareddy@gmail.com>
2023-05-12 14:33:32 +0530

    learn-terraform

---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "aws_ssm_parameter" "parameters" {
+  count = length(var.parameters)
+  name  = var.parameters[count.index].name
+  value = var.parameters[count.index].value
+  type  = "String"
+  key_id = "80ec9bf2-87a9-488d-9663-23ac476001ca"
+}
