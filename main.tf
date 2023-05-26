8264a03aafb537f663498a9f4ca1435f497d3eab
prasanna kumar <prasannaakkalareddy@gmail.com>
2023-05-26 17:24:49 +0530

    roboshop - ansible

--- main.tf
+++ main.tf
@@ -5,3 +5,12 @@ resource "aws_ssm_parameter" "parameters" {
   type  = "String"
   key_id = "80ec9bf2-87a9-488d-9663-23ac476001ca"
 }
+
+resource "aws_ssm_parameter" "passwords" {
+  count = length(var.passwords)
+  name  = var.passwords[count.index].name
+  value = var.passwords[count.index].value
+  type  = "SecureString"
+  key_id = "80ec9bf2-87a9-488d-9663-23ac476001ca"
+}
+
