1bc5b117e7ba39fd63c9c6f60230c0f782dfe0ad
prasanna kumar <prasannaakkalareddy@gmail.com>
2023-07-28 22:19:27 +0530

    roboshop - ansible

--- init-container.sh
+++ init-container.sh
@@ -1,1 +1,5 @@
-aws ssm get-parameters  --names prod.payment.AMQP_HOST | jq .Parameters[].Value | sed -e 's/"//g'
+for param in ${PARAMS}; do
+  VAR=$(echo $param | awk -F . '{print $NF}' | tr '[:lower:]' '[:upper:]' )
+  VAL=$(aws ssm get-parameters  --names $param | jq .Parameters[].Value | sed -e 's/"//g')
+  echo $VAR=$VAL
+done
