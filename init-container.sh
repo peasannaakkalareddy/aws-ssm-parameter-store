fe76f211ec1c1c1517a9fb9e15370f0a361182dc
prasanna kumar <prasannaakkalareddy@gmail.com>
2023-07-28 22:31:52 +0530

    roboshop - ansible

--- init-container.sh
+++ init-container.sh
@@ -2,4 +2,5 @@ for param in ${PARAMS}; do
   VAR=$(echo $param | awk -F . '{print $NF}' | tr '[:lower:]' '[:upper:]' )
   VAL=$(aws ssm get-parameters  --names $param | jq .Parameters[].Value | sed -e 's/"//g')
   echo $VAR=$VAL
+  echo $VAR=$VAL >>/params/params
 done
